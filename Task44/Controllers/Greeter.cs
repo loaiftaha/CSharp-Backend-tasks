9a9783d95692091948bbbe66a14892e8c4820e2d
loaiftaha <loai.f.taha@gmail.com>
2025-12-24 02:05:07 +0300

    Task 44

---
+++ Task44/Controllers/Greeter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Task44.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class Greeter : Controller
+    {
+        [HttpGet]
+        public string Get([FromQuery] string? name)
+        {
+            var nameRes = string.IsNullOrWhiteSpace(name) ? "Anonymous" : name;
+            return $"Hello {nameRes}!" ;
+
+        }
+    }
+
+
+}
