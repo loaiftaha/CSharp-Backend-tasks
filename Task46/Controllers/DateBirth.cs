2b143e8a62c8fee67de251dc06d4c99b36033689
loaiftaha <loai.f.taha@gmail.com>
2025-12-24 03:24:39 +0300

    Task 46

---
+++ Task46/Controllers/DateBirth.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task46.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BirthDate : Controller
+    {
+        [HttpPost]
+        public string Get([FromForm] string? name, [FromForm] int? years, [FromForm] int? months, [FromForm] int? days)
+        {
+            var nameRes = string.IsNullOrWhiteSpace(name) ? "Anonymous" : name;
+
+            if (years is not null && months is not null && days is not null)
+            {
+                
+
+                int ageY = (System.DateTime.Now.Year - (int)years);
+
+                if (System.DateTime.Now.Month < months || System.DateTime.Now.Month == months && System.DateTime.Now.Day < days)
+                {
+                    ageY--;
+                }
+
+                return $"Hello {nameRes}. Your age is {ageY} Years!";
+
+
+            }
+            else
+            {
+                return $"Hello {nameRes}, I can’t calculate your age without knowing your birthdate!";
+            }
+
+
+        }
+    }
+}
+
+
---
+++ Task46/Controllers/Greeter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task46.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Greeter : Controller
+    {
+        [HttpPost]
+        public string Get([FromForm] string? name)
+        {
+            var nameRes = string.IsNullOrWhiteSpace(name) ? "Anonymous" : name;
+            return $"Hello {nameRes}!";
+        }
+    }
+}
